8fc524c6b02fb99f293fef0ec50de5f542b1da1f
Do Duy Dong <doduydong.97d@gmail.com>
2024-03-01 22:36:37 +0700

    Init repository

---
+++ javaPK/java00/Check_Environment.java
@@ -0,0 +1,13 @@
+package java00;
+
+public class Check_Environment {
+
+	public static void main(String[] args) {
+		// OS
+		System.out.println(System.getProperty("os.name"));
+
+		// JDK
+		System.out.println(System.getProperty("java.version"));
+	}
+
+}
