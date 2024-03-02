0e5216d67eebc4aeeb9221c80c381e907608fd23
Do Duy Dong <doduydong.97d@gmail.com>
2024-03-02 23:03:15 +0700

    Variables and Data Types

---
+++ javaPK/java01/J_01_Variables.java
@@ -0,0 +1,26 @@
+package java01;
+
+public class J_01_Variables {
+
+	public static void main(String[] args) {
+		int a; // Declare
+		a = 10; // Assign
+		System.out.println("a = " + a);
+
+		int b = 20; // Declare & Assign
+		System.out.println("b = " + b);
+
+		int c, d;
+		c = 30;
+		d = 40;
+		System.out.println("c = " + c);
+		System.out.println("d = " + d);
+
+		int e = 50, f = 60;
+		System.out.println("e = " + e);
+		System.out.println("f = " + f);
+
+		System.out.println(a + "| " + b + "| " + c + "| " + d + "| " + e + "| " + f);
+	}
+
+}
---
+++ javaPK/java01/J_02_Data_Types.java
@@ -0,0 +1,34 @@
+package java01;
+
+public class J_02_Data_Types {
+
+	public static void main(String[] args) {
+		byte bNum = 123;
+		System.out.println(bNum);
+
+		short sNum = 12345;
+		System.out.println(sNum);
+
+		int iNum = 1234567;
+		System.out.println(iNum);
+
+		long lNum = 123456789;
+		System.out.println(lNum);
+
+		float fNum = 1.2345678f;
+		System.out.println(fNum);
+
+		double dNum = 1.2345678910111213;
+		System.out.println(dNum);
+
+		char letter = 'D';
+		System.out.println(letter);
+
+		boolean status = true;
+		System.out.println(status);
+
+		String name = "Dong";
+		System.out.println(name);
+	}
+
+}
