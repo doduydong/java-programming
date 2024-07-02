b2ded228afdc2ee0ab7094139a7b357f54ae531b
Do Duy Dong <doduydong.97d@gmail.com>
2024-07-02 11:34:41 +0700

    Variables and Data Types

---
+++ javaPK/java02/J_01_Variables.java
@@ -0,0 +1,28 @@
+package java02;
+
+public class J_01_Variables {
+
+	public static void main(String[] args) {
+		// #1
+		int a; // declaration
+		a = 10; // assignment
+		System.out.println("a = " + a);
+
+		// #2
+		int b = 20; // declaration & assignment
+		System.out.println("b = " + b);
+
+		// #3
+		int c, d;
+		c = 30;
+		d = 40;
+		System.out.println("c = " + c);
+		System.out.println("d = " + d);
+
+		// #4
+		int e = 50, f = 60;
+		System.out.println("e = " + e);
+		System.out.println("f = " + f);
+	}
+
+}
---
+++ javaPK/java02/J_02_Data_Types.java
@@ -0,0 +1,36 @@
+package java02;
+
+public class J_02_Data_Types {
+
+	public static void main(String[] args) {
+		// Primitive Data Types
+		byte nNumber = 123;
+		System.out.println(nNumber);
+
+		short sNumber = 12345;
+		System.out.println(sNumber);
+
+		int iNumber = 1234567;
+		System.out.println(iNumber);
+
+		long lNumber = 123456789;
+		System.out.println(lNumber);
+
+		float fNumber = 1.2345678f;
+		System.out.println(fNumber);
+
+		double dNumber = 1.2345678910111213;
+		System.out.println(dNumber);
+
+		char letter = 'D';
+		System.out.println(letter);
+
+		boolean status = true;
+		System.out.println(status);
+
+		// Non-Primitive Data Types
+		String name = "Dong";
+		System.out.println(name);
+	}
+
+}
