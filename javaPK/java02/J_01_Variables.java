22a3f856570b3f6c1ffbf1642d7cc6055d7a9173
doduydong <doduydong@DODUYDONG>
2024-08-08 22:08:13 +0700

    Variables and data types

---
+++ javaPK/java02/J_01_Variables.java
@@ -0,0 +1,24 @@
+package java02;
+
+public class J_01_Variables {
+
+	public static void main(String[] args) {
+		int a;
+		a = 10;
+		System.out.println(a);
+
+		int b = 20;
+		System.out.println(b);
+
+		int c, d;
+		c = 30;
+		d = 40;
+		System.out.println(c);
+		System.out.println(d);
+
+		int e = 50, f = 60;
+		System.out.println(e);
+		System.out.println(f);
+	}
+
+}
